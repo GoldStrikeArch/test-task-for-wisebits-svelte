d72d0bce2d8c277fcdfd7e7148df829f33162027
GoldStrikeArch <arthasmontano@mail.ru>
2023-06-11 19:42:11 +0300

    Added Card and CardList components

---
+++ src/components/Card.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+  import type { GetRandomCoffeeResponse } from "../api/coffee";
+  import CoffeeImage from "./CoffeeImage.svelte";
+
+  import Notes from "./Notes.svelte";
+
+  export let coffee: GetRandomCoffeeResponse;
+</script>
+
+{#if coffee.error}
+  <p style="color: red">Couldn't fetch coffee :(</p>
+{:else}
+  <section>
+    <CoffeeImage />
+    <div>
+      <h4>{coffee.blend_name}</h4>
+      <p>{coffee.origin}</p>
+      <p>{coffee.variety}</p>
+      <p>{coffee.intensifier}</p>
+      <Notes notes={coffee.notes} />
+    </div>
+  </section>
+{/if}
+
+<style lang="less">
+  section {
+    width: 320px;
+    overflow-y: hidden;
+    overflow-x: auto;
+    background-color: #fff;
+    border-radius: 6px;
+    box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.12);
+  }
+
+  div {
+    padding: 2rem 1.8rem;
+  }
+
+  h4 {
+    font-size: 1.5rem;
+    line-height: 1.5rem;
+    margin-bottom: 1rem;
+  }
+
+  p {
+    font-size: 1rem;
+    line-height: 1.4rem;
+  }
+</style>
---
+++ src/components/CardList.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import { afterUpdate } from "svelte";
+  import { coffeeAsyncList } from "../stores/coffeeAsyncList";
+  import Card from "./Card.svelte";
+
+  afterUpdate(() => {
+    window.scrollTo(0, document.body.scrollHeight);
+  });
+</script>
+
+<main>
+  {#each $coffeeAsyncList.coffees as coffee}
+    <Card {coffee} />
+  {/each}
+</main>
+
+<style lang="less">
+  main {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 16px;
+  }
+</style>
